787748eccf718da3e0846d800b62f92268ca7276
Valentina <oorhmo@gmail.com>
2022-10-30 01:58:29 +0100

    Profile

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { Avatar } from 'primereact/avatar';
+
+const App = () => {
+  return (
+    <div>
+      <Avatar image="profile.jpg" shape="circle" size="x-large"/>
+    </div>
+  );
+}
+
+export default App;
