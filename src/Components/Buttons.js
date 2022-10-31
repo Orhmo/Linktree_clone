576a1f6af81f4f81e5f9b8a058b42146502591cd
Valentina <oorhmo@gmail.com>
2022-11-01 00:59:34 +0100

    subtext

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button } from 'primereact/button';
 
+import Tooltip from '@mui/material/Tooltip';
+
 const Buttons = () =>{
 
 
@@ -9,44 +11,53 @@ const Buttons = () =>{
     <div class="flex justify-content-center">
     <div className="links flex flex-column align-items-center">
 
+    <Tooltip title="My twitter profile">
     <a href="https://twitter.com/ooh_jhay" target="_blank" id="btn__twitter"  rel="noreferrer" >
-    <Button text="My twitter Profile"
-    className="twitter p-2 m-2 " aria-label="Twitter">
+    <Button className="twitter p-2 m-2 " aria-label="Twitter">
         <span className="px-3">Twitter Link</span>
       </Button>
       </a>
+    </Tooltip>
 
+    <Tooltip title="Meet the Zuri Team">
     <a href="https://training.zuri.team/" target="_blank" id="btn__zuri"  rel="noreferrer">
-    <Button text="Meet the Zuri Team" className="zuri p-3 m-2" aria-label="Zuri">
+    <Button className="zuri p-3 m-2" aria-label="Zuri">
         <span className="px-3">Zuri Team</span>
       </Button>
       </a>
+    </Tooltip>
 
+    <Tooltip title="Get access to amazing design and programming books.">
     <a href="http://books.zuri.team" target="_blank" id="books"  rel="noreferrer">
-    <Button text="Get access to amazing design and programming books." className="books p-3 m-2" aria-label="Books">
+    <Button className="books p-3 m-2" aria-label="Books">
         <span className="px-3">Zuri Books</span>
       </Button>
       </a>
+    </Tooltip>
 
+    <Tooltip title="Are you a beginner? Would you like to learn Python without a programming experience.Then, this book is just for you.">
     <a href="https://books.zuri.team/python-for-beginners?ref_id=<orhmo>" target="_blank" id="book__python"  rel="noreferrer">
-    <Button text="Are you a beginner? Would you like to learn Python without a programming experience.Then, this book is just for you." className="python p-3 m-2 justify-content-center" aria-label="Python">
+    <Button className="python p-3 m-2 justify-content-center" aria-label="Python">
         <span className="px-3">Python Books</span>
       </Button>
       </a>
+    </Tooltip>
 
+    <Tooltip title="Think you got what it take?">
     <a href="https://background.zuri.team" target="_blank" id="pitch"  rel="noreferrer">
-
-    <Button text="Think you got what it take?" className="pitch p-3 m-2" aria-label="Pitch">
+    <Button className="pitch p-3 m-2" aria-label="Pitch">
         <span className="px-3">Background Check for Coders </span>
       </Button>
       </a>
+    </Tooltip>
 
-    <a text=" Take your designs to the next level,Get a copy of this amazing guide." href="https://books.zuri.team/design-rules" target="_blank" id="book__design"  rel="noreferrer">
-    <Button
-     className="design p-3 m-2" aria-label="Design">
+    <Tooltip title=" Take your designs to the next level,Get a copy of this amazing guide.">
+    <a href="https://books.zuri.team/design-rules" target="_blank" id="book__design"  rel="noreferrer">
+    <Button className="design p-3 m-2" aria-label="Design">
         <span className="px-3">Design Books</span>
       </Button>
       </a>
+    </Tooltip>
 
     </div>
     </div>
